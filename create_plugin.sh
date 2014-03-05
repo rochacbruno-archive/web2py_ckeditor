c114af7f7c403c7f444c214911c57d5b9fe6c301
timrichardson <tim@tim-richardson.net>
2014-03-05 14:44:35 +1100

    update create_plugin.sh

--- create_plugin.sh
+++ create_plugin.sh
@@ -5,4 +5,4 @@
 # modules/plugin_ckeditor.py
 # static/plugin_ckeditor
 # views/plugin_ckeditor
-tar czvf web2py.plugin.ckeditor.w2p --exclude='*.sh' *
+tar czvf web2py.plugin.ckeditor.w2p --exclude='*.sh' controllers/plugin_ckeditor.py modules/plugin_ckeditor.py static/plugin_ckeditor views/plugin_ckeditor
